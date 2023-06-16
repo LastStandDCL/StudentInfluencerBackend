72ac0903bbdd66f652c93108e37582b96b5e40a5
chenhong <1875435468@qq.com>
2023-06-16 12:23:53 +0800

    完成视频上传，封面截取，文本上传，图片上传

--- src/main/java/com/backend/last_stand/service/impl/UploadServiceImpl.java
+++ src/main/java/com/backend/last_stand/service/impl/UploadServiceImpl.java
@@ -44,7 +44,7 @@ public class UploadServiceImpl implements UploadService, WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler(virtualUploadPath + "/Text/**").addResourceLocations("file:" + FILE_ADDRESS + "Text/");
 		registry.addResourceHandler(virtualUploadPath + "/Img/**").addResourceLocations("file:" + FILE_ADDRESS + "Img/");
-
+		registry.addResourceHandler(virtualUploadPath + "/Video/**").addResourceLocations("file:" + FILE_ADDRESS + "Video/");
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 	}
 
@@ -53,59 +53,125 @@ public class UploadServiceImpl implements UploadService, WebMvcConfigurer {
 	 * 视频文件上传
 	 */
 	@Override
-	public ResponseResult uploadVideo(MultipartFile file, HttpServletRequest request){
-		Map<String, Object> resultMap=new HashMap<String, Object>();
-		
-		String basePath = request.getScheme() + "://" + request.getServerName()
-        + ":" + request.getServerPort() + FILE_ADDRESS;
-
-		//获取当前时间
-		Long time = System.currentTimeMillis();
-
+	public ResponseResult uploadVideo(MultipartFile multipartFile, HttpServletRequest req){
+		if (multipartFile.isEmpty()) {
+			return new ResponseResult<>(400, "文件内容为空");
+		}
 		ResponseResult result = new ResponseResult();
+		//获取客户端系统中文件的原始名称
+		String originName = multipartFile.getOriginalFilename();
+		System.out.println("用户上传文件originName:  " + originName);
 
-		//文件原始名称
-		String fileName = file.getOriginalFilename();
+		//从最后一个.开始截取截取fileName的后缀名
+		String end = originName.substring(originName.lastIndexOf("."));
 
-		//从最后一个.开始截取。截取fileName的后缀名
-		String suffixName = fileName.substring(fileName.lastIndexOf("."));
 
-		//文件新名称,使用时间戳加上后缀命名，这里加上上传人的uid比较好 TODO
-		String newFileName = time+suffixName;
-		
-		String filePath = FILE_ADDRESS + newFileName;
-		File newFile = new File(filePath);
-		//判断目标文件所在目录是否存在
-		if(!newFile.getParentFile().exists()){
-			//如果目标文件所在的目录不存在，则创建父目录
-			newFile.getParentFile().mkdirs();
+		//判断结尾是不是上传pdf，html, md
+		if (!originName.endsWith(".mp4")) {
+			result.setCode(400);
+			result.setMsg("视频类型不符合要求");
+			return result;
 		}
 
-		//视频上传保存url
-		String videoUrl = basePath + newFileName;
 
-		//视频封面图处理
-		String newImgName = time+".jpg";
-		String framefile = FILE_ADDRESS + newImgName;
-		//图片最终位置路径
-		String imgUrlSave = basePath+newImgName;
-		//视频截取封面图
-		String imgUrl= FrameGrabberKit.getVedioImg(videoUrl, framefile, imgUrlSave);
-		
-		//将内存中的数据写入磁盘
+		//防止重名，给文件重新命名，生成一个随机id
+		String newName = UUID.randomUUID().toString().replace("-", "") + end;
+		System.out.println("文件重命名名称为:" + newName);
+
+		//路径不存在就创建
+		File file = new File(FILE_ADDRESS + "Video/", newName);
+
+		//如果存放路径的父目录不存在，就创建它
+		if (!file.getParentFile().exists()) {
+			file.getParentFile().mkdirs();
+		}
+		System.out.println(file.getAbsoluteFile());
+
 		try {
-			file.transferTo(newFile);
-			result.setCode(200);
-			result.setMsg("视频上传成功");
-			resultMap.put("videoUrl", videoUrl);
-			resultMap.put("imgUrl", imgUrl);
-			System.out.println("上传的文件名为："+fileName+",后缀名为："+newFileName);
+			multipartFile.transferTo(file.getAbsoluteFile());
+			//获取url
+			String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort()
+					+ virtualUploadPath + "/Video/" + newName;
 
+			System.out.println("视频存放路径" + url);
+			//视频封面图处理
+			String newImgName = UUID.randomUUID().toString().replace("-", "") +".jpg";
+			String framefile = FILE_ADDRESS +  "Img/" + newImgName;
+			System.out.println("封面图片地址:" +  framefile);
+			String Imggeturl = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort()
+					+ virtualUploadPath + "/Img/" + newImgName;
+			System.out.println("Imggeturl" + Imggeturl);
+
+			//视频截取封面图
+			String imgUrl= FrameGrabberKit.getVedioImg(url, framefile, framefile);
+
+			//文件上传成功给前端返回提示信息，并且返回重命名的文件名称
+			result.setCode(200);
+			result.setMsg("上传视频成功");
+			HashMap<String, Object> mp = new HashMap<>();
+			mp.put("newFileName", newName);
+			mp.put("url", url);
+			mp.put("Imggeturl", Imggeturl);
+			result.setData(mp);
 		} catch (IOException e) {
-			result.setCode(202);
+			result.setCode(400);
 			result.setMsg("视频上传失败");
 		}
 		return result;
+
+
+//		Map<String, Object> resultMap=new HashMap<String, Object>();
+//
+//		String basePath = request.getScheme() + "://" + request.getServerName()
+//        + ":" + request.getServerPort() + FILE_ADDRESS;
+//
+//		//获取当前时间
+//		Long time = System.currentTimeMillis();
+//
+//		ResponseResult result = new ResponseResult();
+//
+//		//文件原始名称
+//		String fileName = file.getOriginalFilename();
+//
+//		//从最后一个.开始截取截取fileName的后缀名
+//		String suffixName = fileName.substring(fileName.lastIndexOf("."));
+//
+//		//文件新名称,使用时间戳加上后缀命名，这里加上上传人的uid比较好
+//		String newFileName = time+suffixName;
+//
+//		String filePath = FILE_ADDRESS + newFileName;
+//		File newFile = new File(filePath);
+//		//判断目标文件所在目录是否存在
+//		if(!newFile.getParentFile().exists()){
+//			//如果目标文件所在的目录不存在，则创建父目录
+//			newFile.getParentFile().mkdirs();
+//		}
+//
+//		//视频上传保存url
+//		String videoUrl = basePath + newFileName;
+//
+//		//视频封面图处理
+//		String newImgName = time+".jpg";
+//		String framefile = FILE_ADDRESS + newImgName;
+//		//图片最终位置路径
+//		String imgUrlSave = basePath+newImgName;
+//		//视频截取封面图
+//		String imgUrl= FrameGrabberKit.getVedioImg(videoUrl, framefile, imgUrlSave);
+//
+//		//将内存中的数据写入磁盘
+//		try {
+//			file.transferTo(newFile);
+//			result.setCode(200);
+//			result.setMsg("视频上传成功");
+//			resultMap.put("videoUrl", videoUrl);
+//			resultMap.put("imgUrl", imgUrl);
+//			System.out.println("上传的文件名为："+fileName+",后缀名为："+newFileName);
+//
+//		} catch (IOException e) {
+//			result.setCode(202);
+//			result.setMsg("视频上传失败");
+//		}
+//		return result;
 	}
 
 	/**
@@ -121,9 +187,8 @@ public class UploadServiceImpl implements UploadService, WebMvcConfigurer {
 		String originName = multipartFile.getOriginalFilename();
 		System.out.println("用户上传文件originName:  " + originName);
 
-		String[] split = originName.split("\\.");
-		//分割出来的第二个位置是文件类型
-		String end = split[1];
+		//从最后一个.开始截取截取fileName的后缀名
+		String end = originName.substring(originName.lastIndexOf("."));
 
 		//判断结尾是不是上传pdf，html, md
 		if (!originName.endsWith(".jpg") && !originName.endsWith(".jpeg") && !originName.endsWith(".png")) {
@@ -133,11 +198,11 @@ public class UploadServiceImpl implements UploadService, WebMvcConfigurer {
 		}
 
 		//防止重名，给文件重新命名，生成一个随机id
-		String newName = UUID.randomUUID().toString().replace("-", "")+ "." + end;
+		String newName = UUID.randomUUID().toString().replace("-", "") + end;
 		System.out.println("文件重命名名称为:" + newName);
 
 		//路径不存在就创建
-		File file = new File(FILE_ADDRESS + "/Img", newName);
+		File file = new File(FILE_ADDRESS + "Img/", newName);
 
 		//如果存放路径的父目录不存在，就创建它
 		if (!file.getParentFile().exists()) {
@@ -175,9 +240,8 @@ public class UploadServiceImpl implements UploadService, WebMvcConfigurer {
 		String originName = multipartFile.getOriginalFilename();
 		System.out.println("用户上传文件originName:  " + originName);
 
-		String[] split = originName.split("\\.");
-		//分割出来的第二个位置是文件类型
-		String end = split[1];
+		//从最后一个.开始截取截取fileName的后缀名
+		String end = originName.substring(originName.lastIndexOf("."));
 
 		//判断结尾是不是上传pdf，html, md
         if (!originName.endsWith(".pdf") && !originName.endsWith(".md") && !originName.endsWith(".html")) {
@@ -188,11 +252,11 @@ public class UploadServiceImpl implements UploadService, WebMvcConfigurer {
 
 
 		//防止重名，给文件重新命名，生成一个随机id
-		String newName = UUID.randomUUID().toString().replace("-", "")+ "." + end;
+		String newName = UUID.randomUUID().toString().replace("-", "") + end;
 		System.out.println("文件重命名名称为:" + newName);
 
 		//路径不存在就创建
-		File file = new File(FILE_ADDRESS + "/Text", newName);
+		File file = new File(FILE_ADDRESS + "Text/", newName);
 
 		//如果存放路径的父目录不存在，就创建它
 		if (!file.getParentFile().exists()) {
