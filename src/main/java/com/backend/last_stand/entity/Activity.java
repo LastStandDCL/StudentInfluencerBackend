449f7e5a262f3186264b2d60dfe2360ba8d956b4
bowen <2631702650@qq.com>
2023-06-28 00:18:41 +0800

    改变了Activity的结构

---
+++ src/main/java/com/backend/last_stand/entity/Activity.java
@@ -0,0 +1,56 @@
+package com.backend.last_stand.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author bowen
+ * @since 2023-06-28
+ */
+@Getter
+@Setter
+@Accessors(chain = true)
+@TableName("sys_activity")
+public class Activity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private String activityName;
+
+    private String year;
+
+    private LocalDate begin;
+
+    private LocalDate signupDue;
+
+    private LocalDate materialDue;
+
+    private LocalDate summaryDue;
+
+    private Integer currentComplete;
+
+    private String signUpDescription;
+
+    private String materialDescription;
+
+    private LocalDate speechDue;
+
+    private String speechDescription;
+
+    private String summaryDescription;
+
+    private String endDescription;
+}
