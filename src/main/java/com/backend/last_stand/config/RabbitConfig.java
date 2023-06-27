a6b47766d73c13545df4f247fac5f30d67276b0a
chenhong <1875435468@qq.com>
2023-06-27 14:42:16 +0800

    接入rabbitmq，完成rabbitmq配置

---
+++ src/main/java/com/backend/last_stand/config/RabbitConfig.java
@@ -0,0 +1,36 @@
+package com.backend.last_stand.config;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.transaction.RabbitTransactionManager;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * rabbitmq配置类
+ */
+@Component
+public class RabbitConfig {
+
+    /**
+     * 定义消息转换实例 ，转化成 JSON传输
+     *
+     * @return Jackson2JsonMessageConverter
+     */
+    @Bean
+    public MessageConverter integrationEventMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * 配置启用rabbitmq事务
+     *
+     * @param connectionFactory connectionFactory
+     * @return RabbitTransactionManager
+     */
+    @Bean
+    public RabbitTransactionManager rabbitTransactionManager(CachingConnectionFactory connectionFactory) {
+        return new RabbitTransactionManager(connectionFactory);
+    }
+}
