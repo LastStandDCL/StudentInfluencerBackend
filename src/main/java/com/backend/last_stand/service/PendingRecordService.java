e93c53d4219cd4ccf693d698faabdf7e93517bc7
wangziyang <1477601551@qq.com>
2023-06-29 17:08:43 +0800

    添加了用户上传团队总结报告日志记录、用户上传团队中期报告日志记录服务的实现

--- src/main/java/com/backend/last_stand/service/PendingRecordService.java
+++ src/main/java/com/backend/last_stand/service/PendingRecordService.java
@@ -3,6 +3,8 @@ package com.backend.last_stand.service;
 import com.backend.last_stand.entity.PendingRecord;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Date;
+
 /**
  * <p>
  *  服务类
@@ -13,4 +15,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface PendingRecordService extends IService<PendingRecord> {
 
+    public boolean logTeamReportApplication(Long applicantId, Long teamId, String year, Date recordTime);
+
+    public boolean logMiddleReportApplication(Long applicantId, Long teamId, String year, Date recordTime);
 }
--- src/main/java/com/backend/last_stand/service/impl/PendingRecordServiceImpl.java
+++ src/main/java/com/backend/last_stand/service/impl/PendingRecordServiceImpl.java
@@ -3,8 +3,12 @@ package com.backend.last_stand.service.impl;
 import com.backend.last_stand.entity.PendingRecord;
 import com.backend.last_stand.mapper.PendingRecordMapper;
 import com.backend.last_stand.service.PendingRecordService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * <p>
@@ -17,4 +21,52 @@ import org.springframework.stereotype.Service;
 @Service
 public class PendingRecordServiceImpl extends ServiceImpl<PendingRecordMapper, PendingRecord> implements PendingRecordService {
 
+    private static final int TYPE_MIDDLEREPORT_APPLYCATION = 0;
+
+    private static final int TYPE_TEAMREPORT_APPLYCATION = 1;
+
+    @Autowired
+    private PendingRecordMapper pendingRecordMapper;
+
+    /**
+     * 用户提交团队总结报告日志记录
+     * @param applicantId
+     * @param teamId
+     * @param year
+     * @param recordTime
+     * @return
+     */
+    @Override
+    public boolean logTeamReportApplication(Long applicantId, Long teamId, String year, Date recordTime) {
+        PendingRecord pendingRecord = new PendingRecord();
+        pendingRecord.setTeamId(teamId);
+        pendingRecord.setApplicantId(applicantId);
+        pendingRecord.setRecordTime(recordTime);
+        pendingRecord.setYear(year);
+        pendingRecord.setPendingType(TYPE_TEAMREPORT_APPLYCATION);
+        pendingRecord.setMessage("用户id:" + applicantId + "提交团队id:" + teamId + "的团队总结报告");
+        pendingRecordMapper.insert(pendingRecord);
+        return true;
+    }
+
+    /**
+     * 用户提交团队总结报告日志记录
+     * @param applicantId
+     * @param teamId
+     * @param year
+     * @param recordTime
+     * @return
+     */
+    @Override
+    public boolean logMiddleReportApplication(Long applicantId, Long teamId, String year, Date recordTime) {
+        PendingRecord pendingRecord = new PendingRecord();
+        pendingRecord.setTeamId(teamId);
+        pendingRecord.setApplicantId(applicantId);
+        pendingRecord.setRecordTime(recordTime);
+        pendingRecord.setYear(year);
+        pendingRecord.setPendingType(TYPE_MIDDLEREPORT_APPLYCATION);
+        pendingRecord.setMessage("用户id:" + applicantId + "提交团队id:" + teamId + "的团队中期报告");
+        pendingRecordMapper.insert(pendingRecord);
+        return true;
+    }
 }
